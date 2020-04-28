e3e5b4e0110f3a7fa5a6fcf586aab100aaf27685
vladgladkov <vladgladkov@gmail.com>
2020-04-28 05:15:12 +0300

    Done home task

--- students/vladgladkov/script.js
+++ students/vladgladkov/script.js
@@ -5,13 +5,23 @@ const goods = [
     { title: 'Shoes', price: 300 },
 ];
 
-const getGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-};
+	// Можно сократить при помощи стрелочной функции
 
-const renderGoodsList = list => {
+const getGoodsItem = (title, price) =>
+	`<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+
+const renderGoodsList = (list = [{title: 'Item Title', price: 0}]) => 
+ 	list.forEach(item => document.querySelector('.goods-list').innerHTML += getGoodsItem(item.title, item.price));
+
+ /*{
     const goodsList = list.map(item => getGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList;
-}
+
+    // document.querySelector('.goods-list').innerHTML = goodsList;
+    // Выводим в html массив goodsList в котором после каждого элемента
+    // находится запятая. Отсюда и запятые на странице.
+    // Решается последовательным выводом элементов.
+
+    goodsList.forEach(element => document.querySelector('.goods-list').innerHTML += element);
+}*/
 
 renderGoodsList(goods);
