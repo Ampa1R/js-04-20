21de547de635048b22fc83a953f0ac4c26df0de1
alexshifty67 <57457324+alexshifty67@users.noreply.github.com>
2020-04-29 19:02:48 +0300

    Homework
    Done

---
+++ students/Alex Shtuchkin/project/script.js
@@ -0,0 +1,15 @@
+const goods = [
+    { title: 'Shirt', price: 100 },
+    { title: 'Socks', price: 50 },
+    { title: 'Jacket', price: 200 },
+    { title: 'Shoes', price: 300 },
+];
+const getGoodsItem = (title = 'Товар', price = 0) => {
+    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+};
+
+const renderGoodsList = list => {
+    const goodsList = list.map(item => getGoodsItem(item.title, item.price));
+    document.querySelector('.goods-list').innerHTML = goodsList.join('');
+}
+renderGoodsList(goods);
