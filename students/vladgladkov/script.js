ca71030f77f284576983f9f44ed7b4c96adb9f58
vladgladkov <vladgladkov@gmail.com>
2020-04-30 02:41:38 +0300

    fix homework, using .join('') method

--- students/vladgladkov/script.js
+++ students/vladgladkov/script.js
@@ -5,23 +5,12 @@ const goods = [
     { title: 'Shoes', price: 300 },
 ];
 
-	// Можно сократить при помощи стрелочной функции
-
 const getGoodsItem = (title, price) =>
 	`<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
 
-const renderGoodsList = (list = [{title: 'Item Title', price: 0}]) => 
- 	list.forEach(item => document.querySelector('.goods-list').innerHTML += getGoodsItem(item.title, item.price));
-
- /*{
+const renderGoodsList = (list = [{title: 'Item Title', price: 0}]) => {
     const goodsList = list.map(item => getGoodsItem(item.title, item.price));
-
-    // document.querySelector('.goods-list').innerHTML = goodsList;
-    // Выводим в html массив goodsList в котором после каждого элемента
-    // находится запятая. Отсюда и запятые на странице.
-    // Решается последовательным выводом элементов.
-
-    goodsList.forEach(element => document.querySelector('.goods-list').innerHTML += element);
-}*/
+    document.querySelector('.goods-list').innerHTML = goodsList.join('');
+}
 
 renderGoodsList(goods);
