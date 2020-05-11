70478cd59aac2e1e46934cc2c417c864089ec735
Filipp Vasilev <ampa1r.pl@gmail.com>
2020-05-11 22:04:38 +0300

    add data from api to project

--- project/script.js
+++ project/script.js
@@ -1,3 +1,32 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const sendRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    // console.log('200 !==', xhr.status);
+                    reject(xhr.status);
+                }
+            }
+        }
+
+        xhr.timeout = 15000;
+
+        xhr.ontimeout = () => {
+            console.log('timeout');
+        };
+
+        xhr.open('GET', url, true);
+
+        xhr.send();
+    });
+};
+
 class GoodsItem {
     constructor(title, price) {
         this.title = title;
@@ -14,19 +43,20 @@ class GoodsList {
         this.goods = [];
     }
 
-    fetchGoods() {
-        this.goods = [
-            { title: 'Shirt', price: 100 },
-            { title: 'Socks', price: 50 },
-            { title: 'Jacket', price: 200 },
-            { title: 'Shoes', price: 300 },
-        ];
+    fetchGoods(callback) {
+        sendRequest(`${API}/catalogData.json?page=1&sort=price`)
+            .then(data => {
+                this.goods = data;
+                callback()
+            }).catch(err => {
+                console.log('fetchGoods error status:', err);
+            });
     }
 
     render() {
         let goodsList = '';
-        this.goods.forEach(({ title, price }) => {
-            const goodItem = new GoodsItem(title, price);
+        this.goods.forEach(({ product_name, price }) => {
+            const goodItem = new GoodsItem(product_name, price);
             goodsList += goodItem.render();
         });
         document.querySelector('.goods-list').innerHTML = goodsList;
@@ -34,5 +64,4 @@ class GoodsList {
 }
 
 const list = new GoodsList();
-list.fetchGoods();
-list.render();
+list.fetchGoods(() => list.render());
