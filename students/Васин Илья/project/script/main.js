09bdfb381173cad9327bda32217db9cfc59e31b7
Ilushasan <58609885+Ilushasan@users.noreply.github.com>
2020-05-11 11:03:31 +0300

    HomeTask2
    Done only first and second

--- students/Васин Илья/project/script/main.js
+++ students/Васин Илья/project/script/main.js
@@ -1,21 +1,59 @@
-const goods = [
-    {title: 'Shirt', price: 100},
-    {title: 'Socks', price: 50},
-    {title: 'Jacket', price: 200},
-    {title: 'Shoes', price: 300},
-];
-
-//Для сокращения записи функции можно использовать стрелочную функцию
-
-const getGoodsItem = (title, price) => `<div class ="goods-item"><div class ="goods-pic"></div><h3>${title}</h3><p>${price}</p></div>`;
-
-/**
-Запятые возникают, потому что для перебора массива использовался метод map, который перебирает массив, применяет функцию к каждому элементу массива и возвращает новый массив
-Чтобы это исправить, нужно использовать метод forEach, он так же перебирает массив и применяет функцию к каждому элементу, но новый массив не возвращает.
-Таким образом мы избавимся от запятых.
- */
-const renderGoodsList = list => {
-    list.forEach(item => document.querySelector('.goods-list').innerHTML += getGoodsItem(item.title, item.price));
+class GoodsItem { 
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    render() {
+        return `<div class ="goods-item"><div class ="goods-pic"></div><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    }
 }
 
-renderGoodsList(goods);
+class GoodsList {
+    constructor () {
+        this.goods = [];
+    }
+
+    getGoods() {
+        this.goods = [
+            {title: 'Shirt', price: 100},
+            {title: 'Socks', price: 50},
+            {title: 'Jacket', price: 200},
+            {title: 'Shoes', price: 300},
+        ] 
+    }
+
+    renderGoodsList() {
+        let list = '';
+        this.goods.forEach(({title, price}) => {
+            const goodItem = new GoodsItem(title, price);
+            list += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = list;
+    }
+
+    getTotalPrice() {
+        
+        this.totalPrice = 0;
+        this.goods.forEach(({price}) => this.totalPrice +=price);
+    }
+
+    renderTotalPrice() {
+        document.querySelector('.total-price').innerHTML = this.totalPrice;
+    }
+}
+
+const goodsList = new GoodsList();
+goodsList.getGoods();
+goodsList.renderGoodsList();
+goodsList.getTotalPrice();
+goodsList.renderTotalPrice();
+
+//Другие классы
+/*
+class RemoveFromBasket - удаляет отдельные товары
+class TotalBasket - после клика на товар, он добавляется к корзину
+class Order - сделать заказ
+class Basket - просмотр корзины
+
+
+*/
