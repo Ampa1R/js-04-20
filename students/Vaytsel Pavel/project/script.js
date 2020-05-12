e92e17667660bff82bb921049b75de930e33c2fc
vaytsel <vaytsel@gmail.com>
2020-05-12 18:38:19 +0300

    refactor to classes

--- students/Vaytsel Pavel/project/script.js
+++ students/Vaytsel Pavel/project/script.js
@@ -1,17 +1,49 @@
-const goods = [
-    { title: 'Shirt', price: 100 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 200 },
-    { title: 'Shoes', price: 300 },
-    {}
-];
+class Basket {
+    constructor() {}
+}
+class BasketItem {
+    constructor() {}
+}
 
-const getGoodsItem = ({ title = 'Товар', price = 0 }) =>
-    `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+class GoodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
 
-const renderGoodsList = (list) =>
-    (document.querySelector('.goods-list').innerHTML = list
-        .map((item) => getGoodsItem({ title: item.title, price: item.price }))
-        .join(' '));
+    render() {
+        return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    }
+}
 
-renderGoodsList(goods);
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+
+    fetchGoods() {
+        this.goods = [
+            { title: 'Shirt', price: 100 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 200 },
+            { title: 'Shoes', price: 300 }
+        ];
+    }
+
+    getTotalPrice() {
+        return this.goods.reduce((total, item) => total + item.price, 0);
+    }
+
+    render() {
+        let goodsList = '';
+        this.goods.forEach(({ title, price }) => {
+            const goodItem = new GoodsItem(title, price);
+            goodsList += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = goodsList;
+    }
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
