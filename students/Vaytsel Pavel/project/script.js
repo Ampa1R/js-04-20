0d6de8b4f48f51b47c0d3f47484377b9ea7c9143
vaytsel <vaytsel@gmail.com>
2020-05-19 19:42:23 +0300

    practice5

--- students/Vaytsel Pavel/project/script.js
+++ students/Vaytsel Pavel/project/script.js
@@ -1,92 +1,41 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-const sendRequest = (url) => {
-    return new Promise((resolve, reject) => {
-        let xhr = new XMLHttpRequest();
-
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    resolve(JSON.parse(xhr.responseText));
-                } else {
-                    // console.log('200 !==', xhr.status);
-                    reject(xhr.status);
-                }
-            }
-        };
-
-        xhr.timeout = 15000;
-
-        xhr.ontimeout = () => {
-            console.log('timeout');
-        };
-
-        xhr.open('GET', url, true);
-
-        xhr.send();
-    });
-};
-
-class GoodsItem {
-    constructor(id, title, price) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
+const app = new Vue({
+    el: '#app',
+    data: {
+        goods: [],
+        filteredGoods: [],
+        selectedItems: [],
+        searchText: '',
+        isVisibleCart: false
+    },
+    methods: {
+        async sendRequest(url) {
+            const res = await fetch(url);
+            return res.json();
+        },
+        filterGoods() {
+            const regexp = RegExp(this.searchText, 'i');
+            this.filteredGoods = this.goods.filter(({ product_name }) => regexp.test(product_name));
+        },
+        addToCart(item) {
+            this.selectedItems.includes(item) ? this.removeFromCart(item) : this.selectedItems.push(item);
+        },
+        removeFromCart(item) {
+            const { selectedItems } = this;
+            selectedItems.splice(
+                selectedItems.findIndex((i) => i === item),
+                1
+            );
+        }
+    },
+    async mounted() {
+        try {
+            const goods = await this.sendRequest(`${API}/catalogData.json?page=1&sort=price`);
+            this.goods = goods;
+            this.filteredGoods = goods;
+        } catch (err) {
+            console.log('fetchGoods error status:', err);
+        }
     }
-
-    async addToBasket() {
-        await sendRequest(`${API}/addToBasket.json?data=${this.id}`);
-    }
-    async deleteFromBasket() {
-        await sendRequest(`${API}/deleteFromBasket.json?data=${this.id}`);
-    }
-
-    render() {
-        return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
-    }
-}
-
-class GoodsList {
-    constructor() {
-        this.goods = [];
-    }
-
-    async fetchGoods() {
-        await sendRequest(`${API}/catalogData.json?page=1&sort=price`)
-            .then((data) => {
-                this.goods = data;
-            })
-            .catch((err) => {
-                console.log('fetchGoods error status:', err);
-            });
-    }
-
-    getTotalPrice() {
-        return this.goods.reduce((total, item) => total + item.price, 0);
-    }
-
-    render() {
-        let goodsList = '';
-        this.goods.forEach(({ id_product, product_name, price }) => {
-            const goodItem = new GoodsItem(id_product, product_name, price);
-            goodsList += goodItem.render();
-        });
-        document.querySelector('.goods-list').innerHTML = goodsList;
-    }
-}
-
-const list = new GoodsList();
-list.fetchGoods().then(() => list.render());
-
-class Basket {
-    constructor() {}
-
-    async getBasket() {
-        await sendRequest(`${API}/getBasket.json`);
-    }
-}
-class BasketItem extends GoodsItem {
-    constructor() {
-        super();
-    }
-}
+});
